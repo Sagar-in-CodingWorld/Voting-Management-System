9598c078a723e170ab602ebc5a5904285c6ca590
Sagar <sagarascoder@gmail.com>
2023-02-08 04:26:49 +0530

    spaces add

--- b38/src/c0602/Day.jsx
+++ b38/src/c0602/Day.jsx
@@ -2,11 +2,11 @@
 let day;
 day=prompt("Enter the day");
 let cstyle={color:"Blue"}
-let msg="It is Office. Go to work";
+let msg="It is Office. Go to work ";
 let image="o.jpg";
 document.write("Hi sagar")
 if(day==='sunday'){
-    msg="It is Holiday. We are going to watch movie to RDB Cinema";
+    msg="It is Holiday. We are going to watch movie to RDB Cinema ";
     cstyle.color="Green";
     image="b.jpg";
 }
